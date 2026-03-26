b13641ee52f1564adca78f392e313086e45ec550
Tuguldur Ganbaatar <xganbaatar.tuguldur@contract.t.dentsusoken.com>
2026-03-26 21:34:52 +0900

    mod

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,7 +3,17 @@ declare module '*.png' {
   export default src
 }
 
+declare module '*.PNG' {
+  const src: string
+  export default src
+}
+
 declare module '*.jpg' {
+  const src: string
+  export default src
+}
+
+declare module '*.JPG' {
   const src: string
   export default src
 }
